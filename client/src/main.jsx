1cd9640f0cef363390fb99381bac9aff280defab
kuldeepkumar <mohankuldeep1234@gmail.com>
2024-07-14 09:45:54 +0530

    minor changes in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,21 @@
 import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, useNavigate } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 import { MyProvider } from './context.jsx';
-import { useNavigate } from 'react-router-dom';
 
 function Root() {
+  const navigate = useNavigate();
+
   useEffect(() => {
     const handleUserLeaving = (event) => {
       localStorage.removeItem("userId");
       localStorage.removeItem("ownerId");
       localStorage.removeItem("roomId");
       localStorage.removeItem("username");
-      console.log("user leaving");
+      navigate("/");
+      console.log("User is leaving");
       event.preventDefault();
       event.returnValue = '';
     };
@@ -22,7 +25,7 @@ function Root() {
     return () => {
       window.removeEventListener('beforeunload', handleUserLeaving);
     };
-  }, []);
+  }, [navigate]);
 
   return (
     <React.StrictMode>
@@ -33,6 +36,14 @@ function Root() {
   );
 }
 
+function AppWithRouter() {
+  return (
+    <BrowserRouter>
+      <Root />
+    </BrowserRouter>
+  );
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Root />
+  <AppWithRouter />
 );
