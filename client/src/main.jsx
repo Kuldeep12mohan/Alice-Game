e2213c66a01a72abb7a97c1bf948b4b67431dc20
kuldeepkumar <mohankuldeep1234@gmail.com>
2024-07-14 09:56:50 +0530

    remove navigate

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,31 +1,27 @@
-import React, { useEffect } from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, useNavigate } from 'react-router-dom';
-import App from './App.jsx';
-import './index.css';
-import { MyProvider } from './context.jsx';
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { MyProvider } from "./context.jsx";
 
 function Root() {
-  const navigate = useNavigate();
-
   useEffect(() => {
     const handleUserLeaving = (event) => {
       localStorage.removeItem("userId");
       localStorage.removeItem("ownerId");
       localStorage.removeItem("roomId");
       localStorage.removeItem("username");
-      navigate("/");
-      console.log("User is leaving");
+      console.log("user leaving");
       event.preventDefault();
-      event.returnValue = '';
+      event.returnValue = "";
     };
 
-    window.addEventListener('beforeunload', handleUserLeaving);
+    window.addEventListener("beforeunload", handleUserLeaving);
 
     return () => {
-      window.removeEventListener('beforeunload', handleUserLeaving);
+      window.removeEventListener("beforeunload", handleUserLeaving);
     };
-  }, [navigate]);
+  }, []);
 
   return (
     <React.StrictMode>
@@ -36,14 +32,4 @@ function Root() {
   );
 }
 
-function AppWithRouter() {
-  return (
-    <BrowserRouter>
-      <Root />
-    </BrowserRouter>
-  );
-}
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <AppWithRouter />
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<Root />);
